147ff6732c72ae5cbfd43809968bbb7fe1eab597
ros101 <ar21868@essex.ac.uk>
2021-08-29 09:08:28 +0200

    for codio

--- harvard/collection.py
+++ harvard/collection.py
@@ -1,15 +1,8 @@
 from .reference import Reference
-from typing import List
 
 class Collection():
 
     def __init__(self, name:str, description:str):
         self.name = name
         self.description = description
-        self.references: List = []
-    
-
-#    def __eq__(self, o: object) -> bool:
-#        return (isinstance(o, Collection)
-#            and self._name == o._name
-#            and self._description == o._description)
+        self.references = []
