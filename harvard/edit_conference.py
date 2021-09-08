c956b934e63c02b22cb8a4382df963206011a279
ros101 <ar21868@essex.ac.uk>
2021-09-08 18:03:06 +0200

    fix small bug

--- harvard/edit_conference.py
+++ harvard/edit_conference.py
@@ -8,8 +8,8 @@ class EditConferencePapersReference(EditReference):
         values['conference_title'] = self.prompt_user_for_input('Conference title', reference.conference_title if reference is not None else None)
         values['conference_location'] = self.prompt_user_for_input('Conference location', reference.conference_location if reference is not None else None)
         values['conference_date'] = self.prompt_user_for_input('Conference date', reference.conference_date if reference is not None else None)
-        values['publisher'] = self.prompt_user_for_input('Publisher', reference.publisher if reference is not None else None)
         values['place'] = self.prompt_user_for_input('Place', reference.place if reference is not None else None)
+        values['publisher'] = self.prompt_user_for_input('Publisher', reference.publisher if reference is not None else None)
         values['pages'] = self.prompt_user_for_input('Pages', reference.pages if reference is not None else None)
         return ConferencePapersReference(
             authors = values['authors'],
@@ -18,7 +18,7 @@ class EditConferencePapersReference(EditReference):
             conference_title= values['conference_title'],
             conference_date= values['conference_date'],
             place= values['place'],
-            publisher= values['conference_location'],
+            publisher= values['publisher'],
             conference_location= values['conference_location'],
             pages = values['pages'])
 
