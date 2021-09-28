ae44d9b458e762a6dd653581d9dda1fcde28ed77
ros101 <ar21868@essex.ac.uk>
2021-09-28 10:38:10 +0200

    fix bug

--- harvard/edit_correspondence.py
+++ harvard/edit_correspondence.py
@@ -5,6 +5,7 @@ class EditCorrespondenceReference(EditReference):
 
     def edit(self, reference: CorrespondenceReference = None):
         values = super().edit(reference)
+        values['date'] = self.prompt_user_for_input('on (day)', reference.date if reference is not None else None)
         return CorrespondenceReference(
             authors = values['authors'],
             year = values['year'],
